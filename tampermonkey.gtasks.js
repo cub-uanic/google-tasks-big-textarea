f005b040a3f0019e8ff3b37c2840d4c7c8666c57
Oleg Kostyuk <cub.uanic@gmail.com>
2016-10-03 22:47:02 +0300

    Update tampermonkey.gtasks.js

--- tampermonkey.gtasks.js
+++ tampermonkey.gtasks.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         GTasksBigTextarea
 // @namespace    https://github.com/cub-uanic/google-tasks-big-textarea
-// @version      0.3
+// @version      0.4
 // @description  improve UX when editing tast details
 // @author       Oleg Kostyuk <cub.uanic@gmail.com>
-// @match        https://mail.google.com/tasks/canvas?pli=1
+// @match        https://mail.google.com/tasks/canvas
+// @match 	 https://mail.google.com/tasks/ig
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // @grant        none
 // ==/UserScript==
@@ -19,9 +20,10 @@ function runWithJQuery(callback) {
 function main($){
     var if1=$('iframe');
 	if1.load(function(){
-        setTimeout(function(){
-            $('iframe').contents().find('div.f').find('textarea').attr('rows', 45);
-        },500);
+            setTimeout(function(){
+                $('iframe').contents().find('div.f').find('textarea').attr('rows', 45);
+		$('iframe').contents().find('textarea').attr('rows', 45);
+            },500);
 	});
 }
 
